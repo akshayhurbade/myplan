39c44705a36c6ecdfe7b42b90571e52e4052c816
Akshay Hurbade <akshayhurbade00@gmail.com>
2023-01-04 18:22:11 +0530

    code push

---
+++ src/test/java/com/example/myplan/MyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
